bb10d36256e6e5ac645f24699c1f63b923a70c72
fdiazgilg <fdiazgilg@gmail.com>
2019-11-28 00:34:02 +0100

    Test decrecientes

--- romanos.py
+++ romanos.py
@@ -1,10 +1,12 @@
 
 valores = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+valores5 = {'V':5, 'L':50, 'D':500}
 
 def romano_a_arabigo(numRomano):
     numArabigo = 0
     numRepes = 1
     ultimoCaracter = ''
+
     for letra in numRomano:
         if letra == ultimoCaracter:
             numRepes += 1
@@ -13,11 +15,21 @@ def romano_a_arabigo(numRomano):
         if numRepes > 3:
             return 0
         
+ 
         if letra in valores:
-            numArabigo += valores[letra]
+            if ultimoCaracter == '':
+                pass
+            elif valores[letra] <= valores[ultimoCaracter]:
+                numArabigo += valores[ultimoCaracter]
+            elif ultimoCaracter not in valores5:
+                numArabigo -= valores[ultimoCaracter]
+            else:
+                return 0
         else:
             return 0
-        
+
         ultimoCaracter = letra
-    
+
+    numArabigo += valores[letra]
+
     return numArabigo
