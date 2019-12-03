3ce4646e0856788488a4ad5207fccd22e2b2d695
fdiazgilg <fdiazgilg@gmail.com>
2019-12-04 00:53:33 +0100

    Clase Romanos

--- romanos.py
+++ romanos.py
@@ -1,10 +1,15 @@
-valores = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-valores5 = {'V':5, 'L':50, 'D':500}
+valores = {'I':1, 'V': 5, 'X':10, 'L': 50, 'C':100, 'D': 500, 'M': 1000}
+valores5 = { 'V': 5,  'L': 50,  'D': 500 } 
 simbolosOrdenados = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 
-#Definimos estas tuplas para la transformacion de arabigo a romano
-romanTipo1 = ('M', 'C', 'X', 'I')
-romanTipo5 = ('D', 'L', 'V')
+rangos = {
+    0: {1: 'I', 5 : 'V', 'next': 'X'},
+    1: {1: 'X', 5 : 'L', 'next': 'C'},
+    2: {1: 'C', 5 : 'D', 'next': 'M'},
+    3: {1: 'M', 'next': ''}
+}
+
+
 
 def numParentesis(cadena):
     num = 0
@@ -15,99 +20,141 @@ def numParentesis(cadena):
             break
     return num
 
+
 def contarParentesis(numRomano):
     res = []
     grupoParentesis = numRomano.split(')')
-
     ix = 0
+
     while ix < len(grupoParentesis):
         grupo = grupoParentesis[ix]
         numP = numParentesis(grupo)
         if numP > 0:
-            for j in range(ix+1, numP):
+            for j in range(ix+1, ix+numP):
                 if grupoParentesis[j] != '':
-                    return 0
-            res.append([numP, grupo[numP:]])
-            ix += numP
-        #Este else lo creamos para añadir el ultimo elemento que no lleva parentesis numP = 0
-        else:
-            ix += 1
-            if grupo != '':
-                res.append([0,grupo])
-        
-        #Este if sirve para tratar los casos de parentesis mal formateados
-        if len(res) > 1:
-            for i in range(len(res)-1):
-                if res[i][0] <= res[i+1][0]:
-                    return 0
+                    return 0 #Explota o Falla
+            ix += numP - 1
+
+        if len(grupo[numP:]) > 0:
+            res.append((numP, grupo[numP:]))
+        ix += 1
+   
+    #Este if sirve para tratar los casos de parentesis mal formateados
+    for i in range(len(res)-1):
+        if res[i][0] <= res[i+1][0]:
+            return 0
 
     return res
+
+
+def romano_individual(numRomano):
+    numRepes = 1
+    ultimoCaracter = ''
+    numArabigo = 0
+
+    for letra in numRomano: 
+        #incrementamos el valor del número arábigo con el valor numérico del símbolo romano
+        if letra in valores:
+            numArabigo += valores[letra]
+            if ultimoCaracter == '':
+                pass
+            elif valores[ultimoCaracter] > valores[letra]:
+                numRepes = 1
+            elif valores[ultimoCaracter] == valores[letra]:
+                numRepes += 1
+                if letra in valores5:
+                    return 0
+                if numRepes > 3:
+                    return 0
+            elif valores[ultimoCaracter] < valores[letra]:
+                if numRepes > 1: # No permite repeticiones en las restas
+                    return 0
+                if ultimoCaracter in valores5: #No permite restas de valores de 5 (5, 50, 500)
+                    return 0
+                distancia = simbolosOrdenados.index(letra) - simbolosOrdenados.index(ultimoCaracter) #No permite que se resten unidades de más de un orden
+                if distancia > 2:
+                    return 0
+                numArabigo -= valores[ultimoCaracter]*2
+                numRepes = 1
+        else:  #si el simbolo romano no es permitido devolvemos error (0)
+            return 0
+
+        ultimoCaracter = letra
+
+    return numArabigo
 
 
 def romano_a_arabigo(numRomano):
     numArabigoTotal = 0
     res = contarParentesis(numRomano)
 
-    for i in range(len(res)):
-        numArabigo = 0
-        numRepes = 1
-        ultimoCaracter = ''
-        numRomano = res[i][1]
-        factor = pow(10,3*res[i][0])
-
-        for letra in numRomano: 
-            #incrementamos el valor del número arábigo con el valor numérico del símbolo romano
-            if letra in valores:
-                numArabigo += valores[letra]
-                if ultimoCaracter == '':
-                    pass
-                elif valores[ultimoCaracter] > valores[letra]:
-                    numRepes = 1
-                elif valores[ultimoCaracter] == valores[letra]:
-                    numRepes += 1
-                    if letra in valores5:
-                        return 0
-                    if numRepes > 3:
-                        return 0
-                elif valores[ultimoCaracter] < valores[letra]:
-                    if numRepes > 1: # No permite repeticiones en las restas
-                        return 0
-                    if ultimoCaracter in valores5: #No permite restas de valores de 5 (5, 50, 500)
-                        return 0
-                    distancia = simbolosOrdenados.index(letra) - simbolosOrdenados.index(ultimoCaracter) #No permite que se resten unidades de más de un orden
-                    if distancia > 2:
-                        return 0
-                    numArabigo -= valores[ultimoCaracter]*2
-                    numRepes = 1
-            else:  #si el simbolo romano no es permitido devolvemos error (0)
-                return 0
-
-            ultimoCaracter = letra
-
-        numArabigoTotal += numArabigo * factor
+    for elemento in res:
+        romano = elemento[1]
+        factor = pow(10, 3 * elemento[0])
+        numArabigoTotal += romano_individual(romano) * factor
 
     return numArabigoTotal
 
 
-def arabigo_a_romano(valor):
-    #Variables para el resultado, separar el arabigo y la traduccion a romano de la separacion del arabigo
-    numRomano = ''
-    
-    #Si es menor que 0 o mayor que 3999 retornamos 0
-    if valor > 3999 or valor <= 0:
-        return 0
-    else:
-        cad = str(valor)
+def invertir(cad):
+    return cad[::-1]
 
-    #Recorremos la lista para su transformacion en romanos
-    for i in range(-len(cad), 0):
-        if cad[i] <= '3':
-            numRomano += int(cad[i]) * romanTipo1[i]
-        elif cad[i] == '4':
-            numRomano += romanTipo1[i]+romanTipo5[i]
-        elif cad[i] < '9':
-            numRomano += romanTipo5[i] + (int(cad[i])-5) * romanTipo1[i]
+
+def gruposDeMil(num):
+    cad = str(num)
+    dac = invertir(cad)
+    grupos = []
+
+    rango = 0
+    for i in range(0, len(dac), 3):
+        grupos.append([rango, int(invertir(dac[i:i+3]))])
+        rango += 1
+
+    for i in range(len(grupos)-1):
+        grupoMenor = grupos[i]
+        grupoMayor = grupos[i+1]
+        unidadesMayor = grupoMayor[1] % 10
+
+        if unidadesMayor < 4:
+            grupoMenor[1] = grupoMenor[1] + unidadesMayor * 1000
+            grupoMayor[1] = grupoMayor[1] - unidadesMayor
+
+    grupos.reverse()
+
+    return grupos
+
+
+def arabigo_individual(valor):
+    cad = invertir(str(valor))
+    res = ''
+
+    for i in range(len(cad)-1, -1, -1):
+        digit = int(cad[i])
+        if digit <= 3:
+            res += digit*rangos[i][1]
+        elif digit == 4:
+            res += (rangos[i][1]+rangos[i][5])
+        elif digit == 5:
+            res += rangos[i][5]
+        elif digit < 9:
+            res += (rangos[i][5]+rangos[i][1]*(digit-5))
         else:
-            numRomano += romanTipo1[i]+romanTipo1[i-1]
-    
-    return numRomano
+            res += rangos[i][1]+rangos[i]['next']
+
+    return res
+
+
+def arabigo_a_romano(valor):
+    g1000 = gruposDeMil(valor)
+    romanoGlobal = ''
+
+    for grupo in g1000:
+        rango = grupo[0]
+        numero = grupo[1]
+        if numero > 0:
+            miRomano = '(' * rango + arabigo_individual(numero) + ')'*rango
+        else: 
+            miRomano = ''
+        romanoGlobal += miRomano
+
+    return romanoGlobal
--- romanos_test.py
+++ romanos_test.py
@@ -1,8 +1,8 @@
 import unittest
-from romanos import romano_a_arabigo, arabigo_a_romano, contarParentesis
+from romanos import romano_a_arabigo, arabigo_a_romano, contarParentesis, gruposDeMil
+from romannumber import RomanNumber
 
 class RomanNumberTest(unittest.TestCase):
-
     def test_symbols_romans(self):
         self.assertEqual(romano_a_arabigo('I'), 1)
         self.assertEqual(romano_a_arabigo('V'), 5)
@@ -12,7 +12,7 @@ class RomanNumberTest(unittest.TestCase):
         self.assertEqual(romano_a_arabigo('D'), 500)
         self.assertEqual(romano_a_arabigo('M'), 1000)
         self.assertEqual(romano_a_arabigo('A'), 0)
-
+   
     def test_numeros_crecientes(self):
         self.assertEqual(romano_a_arabigo('XVI'), 16)
         self.assertEqual(romano_a_arabigo('III'), 3)
@@ -20,64 +20,60 @@ class RomanNumberTest(unittest.TestCase):
     def test_no_mas_de_tres_repeticiones(self):
         self.assertEqual(romano_a_arabigo('LXXIII'), 73)
         self.assertEqual(romano_a_arabigo('IIII'), 0)
-        self.assertEqual(romano_a_arabigo('VV'), 0)
+        self.assertEqual(romano_a_arabigo('VVV'), 0)
 
     def test_numeros_decrecientes(self):
+        self.assertEqual(romano_a_arabigo('IX'), 9)
         self.assertEqual(romano_a_arabigo('CMXCIX'), 999)
-        self.assertEqual(romano_a_arabigo('MMCMLXIX'), 2969)
-        self.assertEqual(romano_a_arabigo('MCDXCII'), 1492)
-    
+ 
     def test_restas_no_admiten_repeticiones(self):
         self.assertEqual(romano_a_arabigo('MIIX'), 0)
-        self.assertEqual(romano_a_arabigo('IIX'), 0)
 
     def test_restas_no_admiten_derivados_del_5(self):
-        self.assertEqual(romano_a_arabigo('MVL'), 0)
-        self.assertEqual(romano_a_arabigo('VL'), 0)
-        self.assertEqual(romano_a_arabigo('VC'), 0)
+        self.assertEqual(romano_a_arabigo('VC'),0)
 
     def test_restas_no_admiten_mas_de_un_orden_de_diferencia(self):
-        self.assertEqual(romano_a_arabigo('IL'), 0)
         self.assertEqual(romano_a_arabigo('IC'), 0)
-  
+        self.assertEqual(romano_a_arabigo('IL'), 0)    
+        self.assertEqual(romano_a_arabigo('VL'), 0)    
+
     def test_numeros_mayores_de_3999(self):
         self.assertEqual(romano_a_arabigo('(IV)'), 4000)
-        self.assertEqual(romano_a_arabigo('(MMM)I'), 3000001)
-        self.assertEqual(romano_a_arabigo('((IV))'), 4000000)
         self.assertEqual(romano_a_arabigo('(VII)CMXXIII'), 7923)
-        self.assertEqual(romano_a_arabigo('(((VII)))(DLIII)DCXXXVII'), 7000553637)
+        self.assertEqual(romano_a_arabigo('((VII))(DLIII)DCXXXVII'), 7553637)
 
     def test_procesar_parentesis(self):
-        self.assertEqual(contarParentesis('(IV)'), [[1, 'IV']])
-        self.assertEqual(contarParentesis('((VII))(XL)CCCXXII'), [[2, 'VII'], [1, 'XL'], [0, 'CCCXXII']])
-        self.assertEqual(contarParentesis('(((VII)))(DLIII)DCXXXVII'), [[3, 'VII'], [1, 'DLIII'], [0, 'DCXXXVII']])
+        self.assertEqual(contarParentesis('(IV)'), [(1, 'IV')])
+        self.assertEqual(contarParentesis('((VII))(XL)CCCXXII'), [(2, 'VII'), (1, 'XL'), (0, 'CCCXXII')])
         self.assertEqual(contarParentesis('(VI)((VII))'), 0)
         self.assertEqual(contarParentesis('(VI)((VII)'), 0)
-        self.assertEqual(contarParentesis('VI)VII)'), 0)
-
+        self.assertEqual(contarParentesis('VI)((VII)'), 0)
 
 class ArabicNumberTest(unittest.TestCase):
     def test_unidades(self):
         self.assertEqual(arabigo_a_romano(1), 'I')
         self.assertEqual(arabigo_a_romano(2), 'II')
-        self.assertEqual(arabigo_a_romano(3), 'III')
         self.assertEqual(arabigo_a_romano(4), 'IV')
-        self.assertEqual(arabigo_a_romano(5), 'V')
-        self.assertEqual(arabigo_a_romano(6), 'VI')
-        self.assertEqual(arabigo_a_romano(7), 'VII')
-        self.assertEqual(arabigo_a_romano(8), 'VIII')
-        self.assertEqual(arabigo_a_romano(9), 'IX')
 
     def test_arabic_a_roman(self):
         self.assertEqual(arabigo_a_romano(2123), 'MMCXXIII')
         self.assertEqual(arabigo_a_romano(2444), 'MMCDXLIV')
         self.assertEqual(arabigo_a_romano(3555), 'MMMDLV')
-        self.assertEqual(arabigo_a_romano(3000), 'MMM')
         self.assertEqual(arabigo_a_romano(1678), 'MDCLXXVIII')
         self.assertEqual(arabigo_a_romano(2999), 'MMCMXCIX')
-        self.assertEqual(arabigo_a_romano(3999), 'MMMCMXCIX')
-        self.assertEqual(arabigo_a_romano(4000), 0)
-        self.assertEqual(arabigo_a_romano(-1), 0)
+
+    def test_arabic_a_roman_gt_3999(self):
+        self.assertEqual(arabigo_a_romano(4000), '(IV)')
+        self.assertEqual(arabigo_a_romano(7763147686), '(((VII)))((DCCLX))(MMMCXLVII)DCLXXXVI')
+
+    def test_gruposDe1000(self):
+        self.assertEqual(gruposDeMil(7763147686), [[3, 7], [2, 760],[1, 3147], [0, 686]])
+        self.assertEqual(gruposDeMil(3763142686), [[3, 0], [2, 3760],[1, 3140], [0, 2686]])
+
+class RomanNumberClassTest(unittest.TestCase):
+    def test_create_romannumber(self):
+        nr = RomanNumber(23)
+        self.assertEqual(str(nr), 'XXIII')
 
 
 if __name__ == '__main__':
